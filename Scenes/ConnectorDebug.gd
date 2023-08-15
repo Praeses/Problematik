0672c94752c93c88c129c1f6ec8287d9d27100b5
Glen Wallace <glen.wallace@praeses.com>
2023-08-15 18:41:48 -0500

    Implement basic connectors

---
+++ Scenes/ConnectorDebug.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+signal pulsed(value: bool)
+
+var _state = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_timer_timeout():
+	pulsed.emit(_state)
+	_state = !_state
+	pass # Replace with function body.
